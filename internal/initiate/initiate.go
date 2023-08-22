fcaa84bffe9fbebb4e8d5a1e3e58a2e1271440f4
ronnyren <ronnyren@tencent.com>
2023-08-22 14:54:14 +0800

    [feature] add msi install pack

--- internal/initiate/initiate.go
+++ internal/initiate/initiate.go
@@ -1,8 +1,9 @@
-// Package initiate This package allows us to initiate Time Sensitive components (Like registering the windows service) as early as possible in the startup process
+// Package initiate This package allows us to initiate Time
+// Sensitive components (Like registering the windows service)
+// as early as possible in the startup process
 package initiate
 
-
-var (
-    StopCh = make(chan bool)
-)
-
+// StopCh is used by Windows service.
+//
+//nolint:gochecknoglobals
+var StopCh = make(chan bool)
